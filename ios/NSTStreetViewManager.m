ef20ede5760f69df40ee8e71674047b0cf2be8b7
Bülent Yalman <blntylmn@gmail.com>
2017-10-28 15:41:09 +0300

    Update NSTStreetViewManager.m

--- ios/NSTStreetViewManager.m
+++ ios/NSTStreetViewManager.m
@@ -3,7 +3,7 @@
 //  react-native-streetview
 //
 //  Created by Amit Palomo on 26/04/2017.
-//  Copyright © 2017 Nester.co.il All rights reserved.
+//  Copyright © 2017 Nester.co.il.
 //
 
 #import <Foundation/Foundation.h>
@@ -12,16 +12,7 @@
 
 @import GoogleMaps;
 
-@interface RNTGMSPanoramaView : GMSPanoramaView
-    @property (nonatomic, copy) RCTBubblingEventBlock onChange;
-@end
-
-@interface NSTStreetViewManager : RCTViewManager<GMSPanoramaViewDelegate>
-@end
-
-
-@implementation RNTGMSPanoramaView
-
+@interface NSTStreetViewManager : RCTViewManager
 @end
 
 @implementation NSTStreetViewManager
@@ -29,28 +20,16 @@
 RCT_EXPORT_MODULE()
 
 RCT_CUSTOM_VIEW_PROPERTY(coordinate, CLLocationCoordinate, GMSPanoramaView) {
-    if (json == nil) return;
-    
-    [view moveNearCoordinate:[RCTConvert CLLocationCoordinate2D:json]];
-}
+  if (json == nil) return;
 
-- (void)panoramaView:(RNTGMSPanoramaView *)view didMoveToPanorama:(GMSPanorama *)panorama nearCoordinate:(CLLocationCoordinate2D)coordinate {
-    NSLog(@"onMove");
-    view.onChange(@{@"invalid": @(FALSE)});
+  [view moveNearCoordinate:[RCTConvert CLLocationCoordinate2D:json]];
 }
-- (void)panoramaView:(RNTGMSPanoramaView *)view error:(NSError *)error onMoveNearCoordinate:(CLLocationCoordinate2D)coordinate {
-    NSLog(@"onMove error");
-    view.onChange(@{@"invalid": @(TRUE)});
-}
-
-RCT_EXPORT_VIEW_PROPERTY(onChange, RCTBubblingEventBlock)
 
 RCT_EXPORT_VIEW_PROPERTY(allGesturesEnabled, BOOL)
 
 - (UIView *)view {
-    RNTGMSPanoramaView *panoView = [[RNTGMSPanoramaView alloc] initWithFrame:CGRectZero];
-    panoView.delegate = self;
-    return panoView;
+  GMSPanoramaView *panoView = [[GMSPanoramaView alloc] initWithFrame:CGRectZero];
+  return panoView;
 }
 
 @end
